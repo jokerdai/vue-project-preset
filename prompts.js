efb883062002b42768d9752563de81703ad856e4
daizheng6 <daizheng@jd.com>
2019-07-26 14:54:54 +0800

    Update prompts.js

--- prompts.js
+++ prompts.js
@@ -1,32 +1,32 @@
 module.exports = [
-  // {
-  //   name: 'lang',
-  //   type: 'list',
-  //   message: '选择开发语言使用JavaScript还是TypeScript？',
-  //   choices: [
-  //     {
-  //       name: 'JavaScript',
-  //       value: 'js'
-  //     },
-  //     {
-  //       name: 'TypeScript',
-  //       value: 'ts'
-  //     }
-  //   ],
-  //   default: 'js'
-  // },
+  {
+    name: 'lang',
+    type: 'list',
+    message: '选择开发语言使用JavaScript还是TypeScript？',
+    choices: [
+      {
+        name: 'JavaScript',
+        value: 'js'
+      },
+      {
+        name: 'TypeScript',
+        value: 'ts'
+      }
+    ],
+    default: 'js'
+  },
   {
     name: "theme",
     type: "confirm",
     message: `是否需要切换主题功能？`,
     default: true
   },
-  // {
-  //   name: "i18n",
-  //   type: "confirm",
-  //   message: `是否需要支持国际化？`,
-  //   default: true
-  // },
+  {
+    name: "i18n",
+    type: "confirm",
+    message: `是否需要支持国际化？`,
+    default: true
+  },
   {
     name: "demo",
     type: "confirm",
